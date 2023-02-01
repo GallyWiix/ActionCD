14429c4cf319d736b273b5aa5c52acd70858c04c
Administrator <admin@example.com>
2023-02-01 18:45:04 +0800

    Initial commit

---
+++ src/main/java/com/example/actioncd/ActionCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.actioncd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActionCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActionCdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/actioncd/ActionCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.actioncd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActionCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
